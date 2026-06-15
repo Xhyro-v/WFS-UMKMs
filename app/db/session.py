d06861395efec21f53f187c4e8e0e2bcc1369565
Xhyrov-v <naufalazharxyz12@gmail.com>
2026-06-15 20:28:26 +0700

    Setup PostgreSQL and SQLAlchemy

--- app/db/session.py
+++ app/db/session.py
@@ -1,12 +1,14 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
-from app.core.config import DATABASE_URL_TEST
+from sqlalchemy.orm import sessionmaker
+from app.core.config import DATABASE_URL_TEST, DATABASE_URL
 
-engine = create_engine(
-    DATABASE_URL_TEST, connect_args={"check_same_thread": False}
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
 )
-
-SessionLocal = sessionmaker(bind=engine)
 
 def get_db():
     db = SessionLocal()
