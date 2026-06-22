7783cb558ca961ed9a19c5678d6442e570ed8e74
Xhyrov-v <naufalazharxyz12@gmail.com>
2026-06-22 11:46:30 +0700

    migration models

--- alembic/env.py
+++ alembic/env.py
@@ -8,9 +8,9 @@ from alembic import context
 from app.db.base import Base
 from app.core.config import DATABASE_URL
 from app.models.admin import Admin
-from app.models.content import Content
+from app.models.content import Contents
 from app.models.menu import Menu
-from app.models.gallery import GalleryPhotos
+from app.models.gallery import GalleryPhoto
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
