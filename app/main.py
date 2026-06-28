e29b867fdf6710f139f36fcf127a651a61111b46
Xhyrov-v <naufalazharxyz12@gmail.com>
2026-06-28 12:56:21 +0700

    Feat: Add Admin/Public Galleries endpointx

--- app/main.py
+++ app/main.py
@@ -5,7 +5,9 @@ from sqlalchemy.exc import OperationalError
 from app.core.config import SECRET_KEY, DEBUG
 from app.api.auth import router as auth_router
 from app.api.menu import router as menu_router
+from app.api.gallery import router as gallery_router
 from app.api.content import router as content_router
+from app.api.admin.gallery import router as admin_gallery_router
 from app.errors.error_handler import database_error_handler,validation_error_handler
 from app.dependencies.auth import get_current_admin
 
@@ -26,7 +28,11 @@ app.add_exception_handler(
 
 app.include_router(auth_router)
 app.include_router(menu_router)
+app.include_router(gallery_router)
 app.include_router(content_router)
+
+
+app.include_router(admin_gallery_router)
 
 
 @app.get("/")
