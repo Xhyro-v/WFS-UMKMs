e3e9725172352a867666e2cd919d7fc55ec40b50
Xhyro-v <naufalazharxyz12@gmail.com>
2026-08-02 10:14:33 +0700

    Init Vue frontend

---
+++ frontend/src/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from "vue-router";
+import LoginView from "@/views/LoginView.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      component: LoginView,
+    },
+  ],
+});
+
+export default router;
---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref } from "vue";
+import { login } from "@/services/auth_service";
+
+const email = ref("");
+const password = ref("");
+
+async function handleLogin() {
+  try {
+    const result = await login(
+      email.value,
+      password.value
+    );
+
+    console.log(result);
+  }
+
+  catch (err) {
+    console.error(err);
+  }
+}
+</script>
+
+<template>
+
+<h1>Login</h1>
+
+<input
+v-model="email"
+placeholder="Email"
+/>
+
+<input
+type="password"
+v-model="password"
+placeholder="Password"
+/>
+
+<button @click="handleLogin">
+Login
+</button>
+
+</template>
